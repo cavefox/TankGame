891466a76009cf44ab2c24e04e42176fe342f625
DESKTOP-4B4UJHM\Delicate.Chou <micheal.zhoufeng@gmail.com>
2017-08-20 15:28:01 +0800

    Initial Commit

---
+++ TankWorld/Source/TankWorld/TankWorldGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKWORLD_API ATankWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
